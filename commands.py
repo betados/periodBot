0b305e8270f57ebbee5e2379b8db1afc81f3c7e9
Alvaro <torrestal@gmail.com>
2018-03-05 08:43:10 +0100

    working...

--- commands.py
+++ commands.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+# from models import addPeriod
+from datetime import datetime
 
-def cmd_start(bot, update):
+
+def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
                      text=""""¡Hola! Este bot te ayudará a recordar cuando vienen esos días del mes.
                      Teclee /help para más información
@@ -12,11 +15,37 @@ def cmd_help(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
                      text='¡Hola! Este bot te ayudará a recordar cuando vienen esos días del mes.\
                      Estos son los comandos:\
-                     - /time : Muestra la fecha.\
+                     - /time : Muestra la fecha. \
+                     - /set <days since your last period> : Establece tu uĺtimo periodo para recibir avisos los siguientes.\
                      - /help : Muesta este mensaje de ayuda\
                      ')
 
 
-def cmd_time(bot, update):
+def time(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
                      text='Not implemented')
+
+
+def set(bot, update, args):
+    print("today:", datetime.date.today())
+
+    chat_id = update.message.chat_id
+    print(chat_id, args)
+    try:
+        days = int(args[0])
+        if days < 0:
+            bot.send_message(chat_id=update.message.chat_id,
+                            text='Usa cantidades positivas')
+            return
+
+        addPeriod(chat_id=update.message.chat_id, last=days, time=now)
+        bot.send_message(chat_id=update.message.chat_id, text='Tu calendario se ha actualizado\n\
+        Se te avisará dentro de {days} días'.format(days=28-days))
+
+    except (IndexError, ValueError):
+        bot.send_message(chat_id=update.message.chat_id,
+                         text='Usage: /set <days since your last period>')
+
+
+def alarm(bot, job):
+    bot.send_message(chat_id=job.context, text='Te viene lo rojo')
--- main.py
+++ main.py
@@ -15,8 +15,11 @@ dispatcher = updater.dispatcher
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-dispatcher.add_handler(CommandHandler('start', cmd_start))
+dispatcher.add_handler(CommandHandler('start', start))
+# dispatcher.add_handler(CommandHandler('help', help))
 dispatcher.add_handler(CommandHandler('help', cmd_help))
-dispatcher.add_handler(CommandHandler('time', cmd_time))
+dispatcher.add_handler(CommandHandler('time', time, pass_args=True))
+dispatcher.add_handler(CommandHandler('set', set,
+                                      pass_args=True))
 
 updater.start_polling()
